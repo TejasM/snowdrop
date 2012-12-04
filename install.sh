87d9a9b1a9c0ad575cefd32bae7a4af330a2298c
Tejas <tmehta@redhat.com>
2012-12-04 11:58:12 -0500

    Script working to check if server is already running

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
-/$JBOSS_HOME/bin/jboss-cli.sh -c /extension=org.jboss.snowdrop:add
-#/$JBOSS_HOME/bin/jboss-cli.sh -c /subsystem=snowdrop:add
-exit 0
+/$JBOSS_HOME/bin/jboss-cli.sh -c
+if [ $? == 0 ]
+then
+	/$JBOSS_HOME/bin/jboss-cli.sh -c /extension=org.jboss.snowdrop:add	
+	exit 0
+else
+	echo "Starting Server"
+	/$JBOSS_HOME/bin/domain.sh &
+	PID=$!
+	echo $PID
+	echo "Waiting for server to start"
+	sleep 1m
+	echo "Activating snowdrop on server"
+	/$JBOSS_HOME/bin/jboss-cli.sh -c /extension=org.jboss.snowdrop:add
+	for i in `ps -ef| awk '$3 == '$PID' { print $2 }'`
+	do
+		kill -9 $i
+	done
+	exit 0
+fi
--- startServer.sh
+++
@@ -1,1 +0,0 @@
-/$JBOSS_HOME/bin/domain.sh
