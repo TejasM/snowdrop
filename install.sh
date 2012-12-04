b516e79e3eb574b78639ed38847412e5b9fb7b30
Tejas <tmehta@redhat.com>
2012-12-04 16:49:01 -0500

    test1

--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 ipaddr=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
-/$JBOSS_HOME/bin/jboss-cli.sh --controller=$ipaddr:9999 -c
+$JBOSS_HOME/bin/jboss-cli.sh --controller=$ipaddr:9999 -c
 if [ $? == 0 ]
 then
-	/$JBOSS_HOME/bin/jboss-cli.sh --controller=$ipaddr:9999 -c /extension=org.jboss.snowdrop:add	
+	$JBOSS_HOME/bin/jboss-cli.sh --controller=$ipaddr:9999 -c /extension=org.jboss.snowdrop:add	
 	exit 0
 else
-	/$JBOSS_HOME/bin/jboss-cli.sh -c
+	$JBOSS_HOME/bin/jboss-cli.sh -c
 	if [ $? == 0 ]
 	then
-		/$JBOSS_HOME/bin/jboss-cli.sh -c /extension=org.jboss.snowdrop:add	
+		$JBOSS_HOME/bin/jboss-cli.sh -c /extension=org.jboss.snowdrop:add	
 		exit 0
 	else
 		echo "Starting Server"
-		/$JBOSS_HOME/bin/domain.sh &
+		$JBOSS_HOME/bin/domain.sh &
 		PID=$!
 		echo $PID
 		echo "Waiting for server to start"
 		sleep 1m
 		echo "Activating snowdrop on server"
-		/$JBOSS_HOME/bin/jboss-cli.sh --controller=$ipaddr:9999 -c /extension=org.jboss.snowdrop:add
+		$JBOSS_HOME/bin/jboss-cli.sh --controller=$ipaddr:9999 -c /extension=org.jboss.snowdrop:add
 		for i in `ps -ef| awk '$3 == '$PID' { print $2 }'`
 		do
 			kill -9 $i
