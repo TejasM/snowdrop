09298a42986efe8affffd5f06c97a8d479d4f8e4
Tejas <tmehta@redhat.com>
2012-12-04 14:40:31 -0500

    Detect ip addr

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ ipaddr=$(ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk 
 /$JBOSS_HOME/bin/jboss-cli.sh --controller=$ipaddr:9999 -c
 if [ $? == 0 ]
 then
-	/$JBOSS_HOME/bin/jboss-cli.sh -c /extension=org.jboss.snowdrop:add	
+	exit
+	/$JBOSS_HOME/bin/jboss-cli.sh --controller=$ipaddr:9999 -c /extension=org.jboss.snowdrop:add	
 	exit 0
 else
 	echo "Starting Server"
@@ -12,7 +13,7 @@ else
 	echo "Waiting for server to start"
 	sleep 1m
 	echo "Activating snowdrop on server"
-	/$JBOSS_HOME/bin/jboss-cli.sh -c /extension=org.jboss.snowdrop:add
+	/$JBOSS_HOME/bin/jboss-cli.sh --controller=$ipaddr:9999 -c /extension=org.jboss.snowdrop:add
 	for i in `ps -ef| awk '$3 == '$PID' { print $2 }'`
 	do
 		kill -9 $i
