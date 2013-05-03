87d2ae6d497df7b34d8dbefdad0038e860e87565
Tejas Mehta <tmehta@tmehta-FX6840.(none)>
2013-05-02 21:24:41 -0400

    Custom context handler

---
+++ subsystem-as7/subsystem-as7/src/main/java/org/jboss/spring/deployers/as7/SpringIndexProcessor.java
@@ -0,0 +1,91 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.spring.deployers.as7;
+
+import org.jboss.as.ee.component.EEModuleDescription;
+import org.jboss.as.naming.ServiceBasedNamingStore;
+import org.jboss.as.naming.ValueManagedReferenceFactory;
+import org.jboss.as.naming.context.NamespaceContextSelector;
+import org.jboss.as.naming.deployment.ContextNames;
+import org.jboss.as.naming.deployment.JndiName;
+import org.jboss.as.naming.service.BinderService;
+import org.jboss.as.server.deployment.*;
+import org.jboss.as.server.deployment.annotation.AnnotationIndexUtils;
+import org.jboss.as.server.deployment.module.ResourceRoot;
+import org.jboss.jandex.Index;
+import org.jboss.msc.service.ServiceBuilder;
+import org.jboss.msc.service.ServiceName;
+import org.jboss.msc.service.ServiceTarget;
+import org.jboss.msc.value.InjectedValue;
+import org.jboss.spring.factory.NamedXmlApplicationContext;
+import org.jboss.spring.vfs.VFSResource;
+import org.jboss.vfs.VirtualFile;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+import java.util.Map;
+
+/**
+ * @author Marius Bogoevici
+ */
+public class SpringIndexProcessor implements DeploymentUnitProcessor, ServletContextAttributeListener {
+
+    @Override
+    public void deploy(final DeploymentPhaseContext phaseContext) throws DeploymentUnitProcessingException {
+        Map<ResourceRoot, Index> indexes = AnnotationIndexUtils.getAnnotationIndexes(phaseContext.getDeploymentUnit());
+        for(ResourceRoot root: indexes.keySet()){
+            if(root.getRootName().equals("classes")){
+                SpringDeployment.index = indexes.get(root);
+            }
+        }
+
+    }
+
+    @Override
+    public void undeploy(DeploymentUnit context) {
+
+    }
+
+    @Override
+    public void attributeAdded(ServletContextAttributeEvent servletContextAttributeEvent) {
+        if (WebApplicationContext.
+                ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE.
+                equals(servletContextAttributeEvent.getName())) {
+            System.out.println(servletContextAttributeEvent.getName());
+        }
+    }
+
+    @Override
+    public void attributeRemoved(ServletContextAttributeEvent servletContextAttributeEvent) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void attributeReplaced(ServletContextAttributeEvent servletContextAttributeEvent) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
